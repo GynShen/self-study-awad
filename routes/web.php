204690d5a507c7b33ed8fa8cc616d82fc1e23d74
GynShen <gynshen@gmail.com>
2026-04-16 00:10:11 +0800

    Reorder username route for consistency in routing structure

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/{username}', function($username) {
-    return view('welcome', ['username' => $username]);
-});
-
 Route::view('/contact', 'contact');
 
 Route::get('/about', function () {
     return view('about');
 });
 
+Route::get('/{username}', function($username) {
+    return view('welcome', ['username' => $username]);
+});
