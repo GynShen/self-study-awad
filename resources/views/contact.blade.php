cec11be9534054f86f6e75e7b0193a8d54b863ce
GynShen <gynshen@gmail.com>
2026-04-16 00:09:01 +0800

    Add navigation to all views

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,1 +1,8 @@
+<nav>
+    <li>
+        <ul><a href="/">Home</a></ul>
+        <ul><a href="/about">About Us</ul>
+        <ul><a href="contact">Contact Us</ul>
+    </li>
+</nav>
 <h1>This is contact us page</h1>
