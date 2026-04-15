cb3bcee65160740ca360dd320c58bf8596701849
GynShen <gynshen@gmail.com>
2026-04-16 00:12:23 +0800

    Fix closing tags for About Us and Contact Us links in navigation

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,8 @@
 <nav>
     <li>
         <ul><a href="/">Home</a></ul>
-        <ul><a href="/about">About Us</ul>
-        <ul><a href="contact">Contact Us</ul>
+        <ul><a href="/about">About Us</a></ul>
+        <ul><a href="contact">Contact Us</a></ul>
     </li>
 </nav>
 <h1>This is about us page</h1>
